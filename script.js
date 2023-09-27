3013c7774126601c0009e952d3e4fc258fbb4e9f
Usman Hanif <144654792+iusman09@users.noreply.github.com>
2023-09-27 17:49:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-let input = document.getElementById('box');
+let string = "";
 let buttons = document.querySelectorAll('.btn');
-
-let starting_string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            starting_string = eval(starting_string);
-            input.value = starting_string;
-        }
-
-        else if(e.target.innerHTML == 'C'){
-            starting_string= "";
-            input.value = starting_string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            starting_string = starting_string.substring(0, starting_string.length-1);
-            input.value = starting_string;
-        }
-        else{
-            starting_string += e.target.innerHTML;
-            input.value = starting_string;
-        }
-        
-    })
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
 })
